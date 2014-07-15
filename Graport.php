44b603fc3e4b2d15c65e0f83b5741dd9a8132046
flenczewski <fabian.lenczewski@gmail.com>
2014-07-15 18:27:25 +0200

    Update Graport.php
    added method _getGoogleResult

--- Graport.php
+++ Graport.php
@@ -22,6 +22,27 @@ class Graport
     function __destruct() {
     }
 
+
+    /**
+     * Grab Search Engine Result Page 
+     * 
+     * @param string $googleUrl  - url from $this->queryUrl()
+     * 
+     * @return string
+     */
+    private function _getGoogleResult( $googleUrl ) {
+      	$rC = curl_init();
+    	curl_setopt($rC, CURLOPT_HEADER, 0);
+    	curl_setopt($rC, CURLOPT_RETURNTRANSFER, 1);
+    	curl_setopt($rC, CURLOPT_VERBOSE, 1);
+    	curl_setopt($rC, CURLOPT_REFERER, $this->dataCenter);
+    	curl_setopt($rC, CURLOPT_URL, $googleUrl );
+    	$data = curl_exec($rC);
+    	curl_close($rC);
+
+        return $data;
+    }
+
     /**
      * Prepare URL address
      *
@@ -32,7 +53,7 @@ class Graport
      *
      * @return string url
      */
-    function queryUrl( $query, $lang = 'pl', $num = 10, $start = 0) {
+    public function queryUrl( $query, $lang = 'pl', $num = 10, $start = 0) {
         return 'http://'. $this->dataCenter .'/search?num='. $num .'&hl='. $lang .'&q='. urlencode($query) .'&lr=' . $lang . '&start=' . $start;
     }
 
